af5479293f3b783624850619fb9da640a7f6d817
eddy <edgpena@mailes.ueb.edu.ec>
2022-12-19 19:26:13 -0500

    primer commit

---
+++ referente.js
@@ -0,0 +1,58 @@
+/*let vaso="recipiente"
+alert ("vaso")*/
+
+
+/*let nombre=prompt ("hola dame tu nombre");
+
+alert("hola" + nombre)*/
+/*asigbacion
+/*numero=10;
+numero +=5;
+
+document.write(numero)*/
+/*numero=10;
+numero **=5;
+
+document.write(numero)
+
+aritmeticos
+
+numero=10;
+numero1 =5;
+numero1++;
+resultado=numero1;
+alert(resultado)
+
+saludo="hola pedro";
+pregunta="como estas";
+
+frace=saludo + pregunta;
+
+document.write(frace)
+
+let numero = 45;
+let numero1 = 46;
+
+document.write(numero==numero1) */
+
+numero1=5;
+numero2="6";
+frace=numero2.concat(numero1)
+document.write(frace)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
